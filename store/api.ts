bb0ef06822daae1d9b660c38b6024c53bc460a24
Florien Niyongabo <waka.florien45@gmail.com>
2025-04-11 12:25:36 +0200

    Fix build

--- store/api.ts
+++ store/api.ts
@@ -14,6 +14,6 @@ export const baseAPI = createApi({
       return headers;
     },
   }),
-  tagTypes: ['challenges', 'challenge', 'notifications', 'notification'],
-  endpoints: () => ({}), 
+  tagTypes: ['challenges', 'challenge', 'notifications', 'notification', 'skills', 'categories', 'prizes', 'systemLogs'],
+  endpoints: () => ({}),
 });
